350a5a816ae240c6a309d0e3d5b729c15b9c58bd
Jaagup Siil <jaagsii@greeny.cs.tlu.ee>
2015-10-16 11:11:55 +0300

    separate form for editing

--- edit.php
+++ edit.php
@@ -1,5 +1,19 @@
 <?php
 
 	//edit
-
+	
+	//id mida muudame
+	echo $_GET["edit"]
+	
+	//saada kätte kõige uuemad andmed selle id kohta
+	//numbrimärk ja värv
 ?>
+<h2>Muuda autot</h2>
+ <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+ 
+	<label for="number_plate">Auto nr</label><br>
+  	<input id="number_plate" name="number_plate" type="text" value=""><br><br>
+	<label for="color">Värv</label><br>
+  	<input name="color" type="text"> <value="<?=$color; ?>"><br><br>
+  	<input type="submit" name="update" value="Salvesta">
+  </form>
